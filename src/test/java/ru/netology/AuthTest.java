c81328686d5cf06218634c44147d6def6af2dd30
Viki <masalikavika@yandex.ru>
2022-11-13 19:14:23 +0300

    settings

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,12 @@ class AuthTest {
     void setup() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
+    }
+
+    @AfterEach
+    void memoryClear() {
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
     }
 
     @Test
